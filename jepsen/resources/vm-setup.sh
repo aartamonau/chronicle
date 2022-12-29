c2c206b545e7d52d577550620e41faed43151367
Aliaksey Artamonau <aliaksiej.artamonau@gmail.com>
2022-12-29 01:05:24 -0800

    Use debian testing vagrant.
    Because it has erlang 25 easily available.

--- jepsen/resources/vm-setup.sh
+++ jepsen/resources/vm-setup.sh
@@ -3,8 +3,11 @@
 echo "Running vm-setup script"
 
 apt-get update
-apt-get install -y gcc g++
-snap install --classic erlang
+apt-get install -y gcc g++ erlang-base rebar3
+echo "PasswordAuthentication yes" > /etc/ssh/sshd_config.d/allow_passwords.conf
+systemctl reload sshd.service
+echo vagrant:vagrant | chpasswd
+sed -i '1i PATH="$PATH:/usr/sbin"; export PATH' /home/vagrant/.bashrc
 
 echo "AAAAAAAAAAAAAAAAAAAA" > /home/vagrant/.erlang.cookie
 chown vagrant:vagrant /home/vagrant/.erlang.cookie
